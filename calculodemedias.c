bf49afd03d7c290c2e2fef239a807cca815997d0
luisinhoferraz <ferrazluisinho@gmail.com>
2016-05-04 12:52:43 -0300

    Cadastro de n alunos
    Agora é possível cadastrar mais do que apenas um aluno, respeitando o requisito do cliente: máximo de 35 alunos. Foram adicionados comentários para facilitar o entendimento do código.
    Obs: A quantidade de faltas é recebida por último.

--- calculodemedias.c
+++ calculodemedias.c
@@ -1,31 +1,31 @@
 /* Projeto de Engenharia de Software:
    Calcular a nota final dos alunos e ver se foram aprovados
-   HaverÃ¡ vÃ¡rios alunos cadastrados
+   Haverá vários alunos cadastrados
 */
 
-// 1Âº: Cadastrar os alunos em um vetor de structs (MÃ¡ximo 35)
+// 1º: Cadastrar os alunos em um vetor de structs (Máximo 35)
 //      Nome, RA, faltas, P1, P2, P3 (opcional), trabalho e exame (OK)
 //	    Cadastrar 1 aluno (OK)
-//      Cadastrar n alunos
-// 2Âº: Receber notas e faltas
-//	    4 ou menos faltas (AprovaÃ§Ã£o)
-//	    Mais que 4 faltas (ReprovaÃ§Ã£o por falta)
-// 3Âº: Calcular mÃ©dia parcial
+//      Cadastrar n alunos (OK)
+// 2º: Calcular média parcial
 //	    (P1+P2)*0.45 + T*0.1 ou (P1+P2+P3)*0.3 + T*0.1
-// 4Âº: Verificar Exame
-//	    MÃ©dia < 2.5 (ReprovaÃ§Ã£o)
-//	    2.5 <= MÃ©dia <= 5.9 (Exame)
-//	    MÃ©dia >= 6 (AprovaÃ§Ã£o)
-// 5Âº: Calcular nota final
-//	    Nota = MÃ©dia parcial
-//	    Ou Nota = (MÃ©dia Parcial*2 + Exame*1)/3
-//		Nota >= 5 (AprovaÃ§Ã£o)
-// 6Âº: Exibir aprovaÃ§Ã£o/reprovaÃ§Ã£o
-// 7Âº: OpÃ§Ã£o de relatÃ³rio de aprovaÃ§Ãµes (Requisito inconsciente)
+// 3º: Verificar Exame
+//	    Média < 2.5 (Reprovação)
+//	    2.5 <= Média <= 5.9 (Exame)
+//	    Média >= 6 (Aprovação)
+// 4º: Calcular nota final
+//	    Nota = Média parcial
+//	    Ou Nota = (Média Parcial*2 + Exame*1)/3
+//		Nota >= 5 (Aprovação)
+// 5º: Avaliar faltas
+//	    4 ou menos faltas (Aprovação)
+//	    Mais que 4 faltas (Reprovação por falta)
+// 6º: Exibir aprovação/reprovação
+// 7º: Opção de relatório de aprovações (Requisito inconsciente)
 //	    Alunos aprovados
 //	    Alunos reprovados
 //	    Total de alunos
-//	    Taxas de aprovaÃ§Ã£o e de reprovaÃ§Ã£o
+//	    Taxas de aprovação e de reprovação
 
 #include <stdio.h>
 #include <string.h>
@@ -37,67 +37,103 @@ typedef struct pessoa {
 	float p1, p2, p3, trabalho, exame;
 } Pessoa;
 
-// SerÃ£o cadastrados no mÃ¡ximo 35 alunos
+// Serão cadastrados no máximo 35 alunos
 Pessoa alunos[35];
 
-// FunÃ§Ãµes utilizadas
+// Funções utilizadas
 void cadastrarAluno(Pessoa x[], int i);
+// Teste: void mostrarAlunos (Pessoa x[], int i);
 
 int main(){
-    int i = 0;
+    int n, cont;
 
-    // Cadastrar 1 aluno
-    cadastrarAluno(alunos,i);
+    printf("Deseja cadastrar quantos alunos? ");
+    scanf("%d", &n);
+    getchar();
+
+    while(n > 35){
+        printf("\nCadastre no maximo 35 alunos. Digite novo valor: ");
+        scanf("%d", &n);
+        getchar();
+    }
+
+    // Cadastrar n alunos
+    for(cont = 0; cont < n; cont++){
+            cadastrarAluno(alunos,cont);
+    }
+
+    /* Teste
+    for(cont = 0; cont < n; cont++){
+            mostrarAlunos(alunos,cont);
+    }
+    */
 
 	return 0;
 }
 
+// Função que cadastra aluno em posição i do vetor
 void cadastrarAluno(Pessoa x[], int i){
-    int teste = 0;
+    int teste;
 
-    printf("Nome do aluno %d: ", i+1);
-    fgets(x[i].nome,100,stdin);
+    printf("\n=== CADASTRO DO ALUNO %d ===", i+1);
+
+    printf("\nNome do aluno: "); // Receber nome do aluno
+    fgets(x[i].nome, 100, stdin);
     getchar();
-    // printf("\n%s\n", x[i].nome);
+    // Teste: printf("\n%s\n", x[i].nome);
 
-    printf("\nRA: ");
+    printf("\nRA: "); // Receber RA
     scanf("%d", &x[i].ra);
     getchar();
-    // printf("\n%d\n", x[i].ra);
+    // Teste: printf("\n%d\n", x[i].ra);
 
-    printf("\nNumero de faltas: ");
-    scanf("%d", &x[i].faltas);
-    getchar();
-    // printf("\n%d\n", x[i].faltas);
-
-    printf("\nNota da P1: ");
+    printf("\nNota da P1: "); // Receber nota da P1
     scanf("%f", &x[i].p1);
     getchar();
-    // printf("\n%.2f\n", x[i].p1);
+    // Teste: printf("\n%.2f\n", x[i].p1);
 
-    printf("\nNota da P2: ");
+    printf("\nNota da P2: "); // Receber nota da P2
     scanf("%f", &x[i].p2);
     getchar();
-    // printf("\n%.2f\n", x[i].p2);
+    // Teste: printf("\n%.2f\n", x[i].p2);
 
-    printf("\nO aluno %d fez a P3? (1 para sim, 0 para nÃ£o) ", i+1);
+    printf("\nO aluno fez a P3? (1 para sim, 0 para nao) "); // A P3 é opcional
     scanf("%d", &teste);
     getchar();
 
-    if (teste==1){
-        printf("\nNota da P3: ");
+    if (teste == 1){ // O aluno fez a P3
+        printf("\nNota da P3: "); // Receber nota da P3
         scanf("%f", &x[i].p3);
         getchar();
-        // printf("\n%.2f\n", x[i].p3);
+        // Teste: printf("\n%.2f\n", x[i].p3);
+        printf("\n");
     }
 
-    else {
-        x[i].p3 = -1;
-        // printf("\n%.0f\n", x[i].p3);
+    else { // O aluno não fez a P3
+        x[i].p3 = -1; // Nota negativa para cálculo de média sem P3
+        // Teste: printf("\n%.0f\n", x[i].p3);
     }
 
-    printf("\nNota do trabalho: ");
+    printf("\nNota do trabalho: "); // Receber nota do trabalho
     scanf("%f", &x[i].trabalho);
     getchar();
-    // printf("\n%.2f\n", x[i].trabalho);
+    // Teste: printf("\n%.2f\n", x[i].trabalho);
+
+    printf("\nNumero de faltas: "); // Receber número de faltas
+    scanf("%d", &x[i].faltas);
+    getchar();
+    // Teste: printf("\n%d\n", x[i].faltas);
+    printf("\n");
+}
+
+// Função que imprime todos os dados do aluno (será excluida posteriormente)
+void mostrarAlunos (Pessoa x[], int i){
+    printf("\n=== Aluno %d (posicao %d) ===", i+1, i);
+    printf("\nNome: %s", x[i].nome);
+    printf("\nRA: %d", x[i].ra);
+    printf("\nNota da P1: %.2f", x[i].p1);
+    printf("\nNota da P2: %.2f", x[i].p2);
+    printf("\nNota da P3: %.2f", x[i].p3);
+    printf("\nNota do trabalho: %.2f", x[i].trabalho);
+    printf("\nFaltas: %d\n", x[i].faltas);
 }
